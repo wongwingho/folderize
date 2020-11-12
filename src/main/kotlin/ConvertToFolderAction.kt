7855907bec32a1d103c5815013698130ed56b8a2
James Wong <james.wong@pinnacle-holdings.hk>
2020-11-12 18:04:57 +0800

    Simplified ConvertToFolderAction

--- src/main/kotlin/ConvertToFolderAction.kt
+++ src/main/kotlin/ConvertToFolderAction.kt
@@ -1,18 +1,19 @@
+import com.intellij.icons.AllIcons.General.Error
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.ui.Messages
 
 class ConvertToFolderAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         val file = e.getData(PlatformDataKeys.VIRTUAL_FILE);
         file?.let {
-            val noFileWithSameNameWithoutExtension =
-                file.parent.children.all { it.path == file.path || it.name != file.nameWithoutExtension }
-
-            if (noFileWithSameNameWithoutExtension) {
+            if (file.parent.findChild(it.nameWithoutExtension) == null) {
                 val folderized = file.parent.createChildDirectory(e, file.nameWithoutExtension)
                 file.move(e, folderized)
                 file.rename(e, "index.${file.extension}")
+            } else {
+                Messages.showMessageDialog("${file.nameWithoutExtension} already exist", "Error", Error)
             }
         }
     }
